51be7c04cb6a8a238f5b413743fc5dd2768e85b8
KURDISHIT <82280252+KURDISHIT@users.noreply.github.com>
2021-04-17 18:17:39 +0300

    Update GIFT.py

--- GIFT.py
+++ GIFT.py
@@ -49,7 +49,7 @@ def chk():
   id = "-".join(uuid) 
   print("\n\n\x1b[37;1mYour ID : "+id) 
   try: 
-    httpCaht = requests.get("https://github.com/KURDISHIT/GIFTHACK/commit/5747687cbb8fc289f53934f2bbb927fdf0cdee77.text") 
+    httpCaht = requests.get("https://github.com/KURDISHIT/GIFTHACK").txt
     if id in httpCaht: 
       print("\033[92mYOUR ID IS ACTIVE.........\033[97m") 
       msg = str(os.geteuid()) 
